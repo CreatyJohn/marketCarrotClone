fc4b62888f1a99e63dec4ee7a8b01b76ac9e819c
권준우 <wnsdn300300@gmail.com>
2022-10-01 16:12:12 +0900

    commit 10.01 PM

---
+++ app/src/main/java/com/example/marketcarrot/UserCheckSplash.kt
@@ -0,0 +1,20 @@
+package com.example.marketcarrot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.marketcarrot.databinding.ActivityUsercheckSplashBinding
+
+class UserCheckSplash : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUsercheckSplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_usercheck_splash)
+        binding = ActivityUsercheckSplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+}
