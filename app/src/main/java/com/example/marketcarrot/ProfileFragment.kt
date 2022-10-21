205a878b73b91d325a707bf2afb54394a3b1f69b
CreatyJohn <vmfodzl1125@naver.com>
2022-10-21 12:58:41 +0900

    commit_week_five_10_21

--- app/src/main/java/com/example/marketcarrot/ProfileFragment.kt
+++ app/src/main/java/com/example/marketcarrot/ProfileFragment.kt
@@ -1,21 +1,15 @@
 package com.example.marketcarrot
 
-import android.content.Context
 import android.os.Bundle
-import android.provider.ContactsContract.Profile
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.marketcarrot.databinding.FragmentProfileBinding
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.user.UserApiClient
-import kotlinx.android.synthetic.main.fragment_home.*
-import kotlinx.android.synthetic.main.fragment_profile.*
 import kotlinx.coroutines.flow.asStateFlow
 
 class ProfileFragment : Fragment() {
@@ -30,12 +24,6 @@ class ProfileFragment : Fragment() {
     private val binding get() = _binding!!
     var mainActivity: MainActivity? = null
 
-//     fragment에서 context 사용하는 방법
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-
-        mainActivity = context as MainActivity
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?
@@ -47,7 +35,22 @@ class ProfileFragment : Fragment() {
         // Kakao SDK 초기화
         activity?.let { KakaoSdk.init(it, "21de26b357c402d6376672dcbda85911") }
 
-        KakaoUserBinding.invoke()
+        UserApiClient.instance.me { user, error ->
+            if (error != null) {
+                Log.e(KakaoAuthViewModel.TAG, "사용자 정보 요청 실패", error)
+            }
+            else if (user != null) {
+                Log.i(
+                    KakaoAuthViewModel.TAG, "사용자 정보 요청 성공" +
+                            "\n회원번호: ${user.id}" +
+                            "\n이메일: ${user.kakaoAccount?.email}" +
+                            "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
+                            "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}")
+
+                binding.tvUsername.text = "${user.kakaoAccount?.profile?.nickname}"
+                binding.tvUseremail.text = "${user.kakaoAccount?.email}"
+            }
+        }
 
         binding.btnKakaoLogout.setOnClickListener {
             KakaoLoginCheck(kakaoAuthViewModel)
@@ -58,42 +61,11 @@ class ProfileFragment : Fragment() {
 
     fun KakaoLoginCheck(viewModel: KakaoAuthViewModel){
 
-        val isLoggedIn = viewModel.isLoggedIn.asStateFlow()
+    val isLoggedIn = viewModel.isLoggedIn.asStateFlow()
 
-        val loginStatusInfoTitle = if (isLoggedIn.value) "카카오로 로그아웃하기" else "카카오로 로그인하기"
+    val loginStatusInfoTitle: String = if (isLoggedIn.value) "카카오로 로그아웃하기" else "카카오로 로그인하기"
 
-        binding.btnKakaoLogout.text = "${loginStatusInfoTitle}"
-        when(isLoggedIn.value) {
-            true -> {
-                viewModel.kakaoLogout()
-            }
-            false -> {
-                viewModel.kakaoLogin()
-            }
-        }
-    }
-
-    val KakaoUserBinding: () -> Unit = {
-        UserApiClient.instance.me { user, error ->
-            if (error != null) {
-                Log.e(KakaoAuthViewModel.TAG, "사용자 정보 요청 실패", error)
-            }
-            else if (user != null) {
-                Log.i(
-                    KakaoAuthViewModel.TAG, "사용자 정보 요청 성공" +
-                        "\n회원번호: ${user.id}" +
-                        "\n이메일: ${user.kakaoAccount?.email}" +
-                        "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
-                        "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}")
-
-                binding.tvUsername.text = "${user.kakaoAccount?.profile?.nickname}"
-                binding.tvUseremail.text = "${user.kakaoAccount?.email}"
-            }
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
+    binding.btnKakaoLogout.text = "${loginStatusInfoTitle}"
+    if (isLoggedIn.value) viewModel.kakaoLogin() else viewModel.kakaoLogout()
     }
 }
